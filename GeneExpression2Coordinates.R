3090c67aca6b3ec337432adfdf96c377f04d5480
CarlosAlfonsoG <35814443+CarlosAlfonsoG@users.noreply.github.com>
2018-02-13 23:17:38 -0600

    Create GeneExpression2Coordinates.R

---
+++ GeneExpression2Coordinates.R
@@ -0,0 +1,30 @@
+GeneExpression2Coordinates <- function(BED, GeneExpresion, GeneName){
+    require(GenomicRanges):require(ChIPseeker):require(GenomicFeatures)
+    data <- read.table(BED,header=F)
+    colnames(data) <- c('ID','chr','start', 'end','size','strand', 'Dyak', 'Dpse', 'Dvir', 'PatternInt', 'Pattern')
+    GRangesChipSeeker <- makeGRangesFromDataFrame(data)
+    mcols(GRangesChipSeeker)$Dyak <- data$Dyak
+    mcols(GRangesChipSeeker)$Dpse <- data$Dpse
+    mcols(GRangesChipSeeker)$Dvir <- data$Dvir
+    mcols(GRangesChipSeeker)$PatternExp <- data$PatternInt
+    mcols(GRangesChipSeeker)$Pattern <- data$Pattern
+    dm3 <- makeTxDbFromUCSC(genome = "dm3", tablename ="ensGene")
+    peakAnnot <- annotatePeak(GRangesChipSeeker, tssRegion=c(-3000, 3000), TxDb=dm3)
+    data.frame <- as.data.frame(peakAnnot)
+    colnames(data.frame)[12] <- "ID"
+    Norm_RNAseq <- read.csv("/Users/carlosalfonso/Projects/EnhancerDivergence/Data/Tables/csv/Norm_Gene_exp_all_species.csv", sep = ",", header = T)
+    colnames(Norm_RNAseq)[1] <- "ID"
+    merge(x = data.frame, y = Norm_RNAseq, by = "ID", all = TRUE)
+    Ectoderm <- data.frame[grep("ecto", data.frame$Pattern), ]
+    Endoderm <- data.frame[grep("endo", data.frame$Pattern), ]
+    Mesoderm <- data.frame[grep("meso", data.frame$Pattern), ]
+    Ubiquitous <- data.frame[grep("ubiquitous", data.frame$Pattern), ]
+    GAP <- data.frame[grep("gap", data.frame$Pattern), ]
+    AP <- data.frame[grep("A-P", data.frame$Pattern), ]
+    write.table(data.frame, file=paste0(GeneName, "_AllPatternsGenes.csv"), quote=F, sep=",", row.names=F, col.names=T)
+    write.table(Ectoderm, file=paste0(GeneName, "_EctodermGenes.csv"), quote=F, sep=",", row.names=F, col.names=T)
+    write.table(Endoderm, file=paste0(GeneName, "_EndodermGenes.csv"), quote=F, sep=",", row.names=F, col.names=T)
+    write.table(Mesoderm, file=paste0(GeneName, "_MesodermGenes.csv"), quote=F, sep=",", row.names=F, col.names=T)
+    write.table(Ubiquitous, file=paste0(GeneName, "_UbiquitousGenes.csv"), quote=F, sep=",", row.names=F, col.names=T)
+
+}
